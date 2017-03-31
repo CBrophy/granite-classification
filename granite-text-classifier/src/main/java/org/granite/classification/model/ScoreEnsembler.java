94d8552b507a272a4f298aa43083b7ca14c29568
Charles Brophy <charlesb771@gmail.com>
2017-03-30 19:21:58 -0700

    Move lucene text classifier to branch. Upgrade utilities version.

--- granite-text-classifier/src/main/java/org/granite/classification/model/ScoreEnsembler.java
+++
@@ -1,8 +0,0 @@
-package org.granite.classification.model;
-
-import com.google.common.collect.ImmutableSet;
-
-public abstract class ScoreEnsembler<T extends StringScore> {
-
-    public abstract ImmutableSet<T> ensemble(final Iterable<T> classificationScores);
-}
--- granite-text-classifier/src/main/java/org/granite/classification/model/ScoreFilter.java
+++
@@ -1,8 +0,0 @@
-package org.granite.classification.model;
-
-import com.google.common.collect.ImmutableSet;
-
-public abstract class ScoreFilter<T extends StringScore> {
-
-    public abstract ImmutableSet<T> filter(final Iterable<T> classificationScores);
-}
